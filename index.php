c09d3c0880dfe7d3dd588c1456e3c3519ed765d2
Rheinmir <rheinmir@gmail.com>
2024-03-27 20:54:50 +0700

    .htaccess

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ $action = _ACTION;
 
 
 
+// Check if 'module' parameter in GET request is not empty and of type string.
 if(!empty($_GET['module'])){
     if(is_string($_GET['module'])){
         $module = trim($_GET['module']);
@@ -24,6 +25,7 @@ if(!empty($_GET['action'])){
 // echo $module.'</br>';
 // echo $action.'</br>';
 
+// Checks if a file exists and requires it if so, otherwise requires a 404 error file.
 $path = 'modules/'. $module. '/'.$action.'.php';
 
 if (file_exists($path)) {
